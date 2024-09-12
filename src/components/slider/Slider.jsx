2e30723687f6a24744951be979e536af35a4ef08
Lord of the Metaverse <107515774+mohamadxnadeem@users.noreply.github.com>
2024-09-12 20:06:35 +0000

    home image update

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,79 +1,90 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import { Navigation, Scrollbar, A11y   } from 'swiper';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Navigation, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 
-import imgbg from '../../assets/images/slider/cpt.jpg'
-
+import desktopImage from '../../assets/images/slider/cpt.jpg';
+import mobileImage from '../../assets/images/slider/mobile.jpeg';
 
 const Slider = props => {
-    const data = props.data
+    const [isMobile, setIsMobile] = useState(false);
+    const data = props.data;
+
+    // Function to check if screen width is mobile or desktop size
+    const checkScreenSize = () => {
+        if (window.innerWidth <= 768) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    };
+
+    // Run on initial load and whenever the window is resized
+    useEffect(() => {
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    }, []);
+
     return (
-        <div className="mainslider" >
+        <div className="mainslider">
             <Swiper
-                modules={[Navigation,  Scrollbar, A11y ]}
-                    spaceBetween={0}
-                    slidesPerView={1}
-                    navigation
-                    scrollbar={{ draggable: true }}
-                >
+                modules={[Navigation, Scrollbar, A11y]}
+                spaceBetween={0}
+                slidesPerView={1}
+                navigation
+                scrollbar={{ draggable: true }}
+            >
                 {
                     data.map((item, index) => (
                         <SwiperSlide key={index} className={item.class}>
-                            <SliderItem item={item} />
+                            <SliderItem item={item} isMobile={isMobile} />
                         </SwiperSlide>
-                        
                     ))
                 }
-        </Swiper>
+            </Swiper>
         </div>
     );
-}
+};
 
 Slider.propTypes = {
     data: PropTypes.array.isRequired,
     control: PropTypes.bool,
     auto: PropTypes.bool,
     timeOut: PropTypes.number
-}
-const SliderItem = props => (
-    <div className="flat-title-page" style={{backgroundImage: `url(${imgbg})`}}>
+};
 
-
-       
+const SliderItem = ({ item, isMobile }) => (
+    <div className="flat-title-page" style={{ backgroundImage: `url(${isMobile ? mobileImage : desktopImage})` }}>
         <div className="overlay"></div>
         <div className="swiper-container mainslider home">
             <div className="swiper-wrapper">
                 <div className="swiper-slide">
-                    <div className="slider-item">	
-                        <div className="themesflat-container ">
+                    <div className="slider-item">    
+                        <div className="themesflat-container">
                             <div className="wrap-heading flat-slider flex">
                                 <div className="content">
                                     <h2 className="heading">Luxury Travel Packages to Cape Town</h2>
-                                    {/* <h1 className="heading mb-style"><span className="tf-text s1">We can help arrange everything you need, want and more!</span>                                          
-                                    </h1> */}
-                                    {/* <h1 className="heading mb-style"><span className="tf-text s1">And 5 Attractions you don't want to regret missing.</span>                                          
-                                    </h1> */}
-
-
-                                  
-                                    
                                 </div>
                                 <div className="image">
-                                    <img className="img-bg" src={props.item.imgbg} alt="cape town" />
-                                    <img src={props.item.img} alt="cape town" />
+                                    <img className="img-bg" src={isMobile ? mobileImage : desktopImage} alt="cape town" />
+                                    <img src={item.img} alt="cape town" />
                                 </div>
                             </div>   
-                        </div>					                           
+                        </div>                                  
                     </div>
                 </div>
             </div>
         </div>        
     </div>
-    
-)
+);
+
 export default Slider;
