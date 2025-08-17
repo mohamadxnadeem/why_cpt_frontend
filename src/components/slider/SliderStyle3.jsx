d35362bc5f39df1d783c07f56f26cf11fad32d77
Lord of the Metaverse <107515774+mohamadxnadeem@users.noreply.github.com>
2025-08-17 22:40:41 +0000

    slider

--- src/components/slider/SliderStyle3.jsx
+++ src/components/slider/SliderStyle3.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
@@ -9,10 +9,10 @@ import styled, { keyframes } from 'styled-components';
 // Shimmer animation
 const shimmer = keyframes`
   0% {
-    background-position: -500px 0;
+    background-position: -400px 0;
   }
   100% {
-    background-position: 500px 0;
+    background-position: 400px 0;
   }
 `;
 
@@ -26,81 +26,83 @@ const SliderContainer = styled.div`
 const SlideContainer = styled.div`
   position: relative;
   width: 100%;
-  padding-top: 56.25%; /* 16:9 Aspect Ratio */
+  height: 300px;
   border-radius: 10px;
   overflow: hidden;
 `;
 
 const ShimmerPlaceholder = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #f6f7f8;
+  background-image: linear-gradient(
+    to right,
+    #f6f7f8 0%,
+    #edeef1 20%,
+    #f6f7f8 40%,
+    #f6f7f8 100%
+  );
+  background-repeat: no-repeat;
+  background-size: 800px 100%;
+  animation: ${shimmer} 1.5s infinite linear;
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    to right,
-    #eeeeee 0%,
-    #dddddd 50%,
-    #eeeeee 100%
-  );
-  background-size: 1000px 100%;
-  animation: ${shimmer} 1.5s linear infinite;
-  z-index: 1;
 `;
 
 const ImageStyled = styled.img`
-  position: absolute;
-  top: 0;
-  left: 0;
   width: 100%;
   height: 100%;
   object-fit: cover;
   display: ${(props) => (props.loaded ? 'block' : 'none')};
+  position: absolute;
+  top: 0;
+  left: 0;
   border-radius: 10px;
-  z-index: 2;
 `;
+
+const SliderItem = ({ item }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <SlideContainer>
+      {!loaded && <ShimmerPlaceholder />}
+      <ImageStyled
+        src={item.src}
+        alt="Experience"
+        loaded={loaded}
+        onLoad={() => setLoaded(true)}
+        onError={(e) => {
+          console.error('Image load error:', e.target.src);
+          e.target.src = '/fallback-image.png'; // fallback image if broken
+          setLoaded(true);
+        }}
+      />
+    </SlideContainer>
+  );
+};
 
 const SliderStyle3 = ({ data }) => {
   return (
     <SliderContainer>
-      <section className="flat-cart-item home6 style2 mainslider">
-        <div className="overlay"></div>
-        <div className="themesflat-container">
-          <div className="row">
-            <div className="col-md-12">
-              <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={30}
-                breakpoints={{
-                  0: { slidesPerView: 1 },
-                  767: { slidesPerView: 2 },
-                  991: { slidesPerView: 3 },
-                }}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-              >
-                {data.map((item, index) => {
-                  const [loaded, setLoaded] = useState(false);
-                  return (
-                    <SwiperSlide key={index}>
-                      <SlideContainer>
-                        {!loaded && <ShimmerPlaceholder />}
-                        <ImageStyled
-                          src={item.src}
-                          alt="Experience"
-                          loaded={loaded}
-                          onLoad={() => setLoaded(true)}
-                        />
-                      </SlideContainer>
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={30}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        breakpoints={{
+          0: { slidesPerView: 1 },
+          767: { slidesPerView: 2 },
+          991: { slidesPerView: 3 },
+        }}
+      >
+        {data.map((item, index) => (
+          <SwiperSlide key={index}>
+            <SliderItem item={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </SliderContainer>
   );
 };
